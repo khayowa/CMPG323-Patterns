d5c7725e68ed4e7c8c38bd708e6f2662273efa5a
KHAYO MAJOZI <33143102@student365.msfed.nwu.ac.za>
2022-09-20 18:24:09 +0200

    Initial Commit

---
+++ OrgOffering/OrgOffering/Data/GTMContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using OrgOffering.Models;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace OrgOffering.Data
+{
+    public partial class GTMContext : DbContext
+    {
+        public GTMContext()
+        {
+        }
+
+        public GTMContext(DbContextOptions<GTMContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<Service> Service { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=GTM;Data Source=SIMPHIWE-PC\\SQLEXPRESS");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId)
+                    .HasColumnName("ProductID")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ProductDescription).IsUnicode(false);
+
+                entity.Property(e => e.ProductName).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Service>(entity =>
+            {
+                entity.Property(e => e.ServiceId)
+                    .HasColumnName("ServiceID")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ServiceDescription).IsUnicode(false);
+
+                entity.Property(e => e.ServiceName).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
